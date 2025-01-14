0905845decca972f3cb7eaa14a6b17d71bc781a6
pidberezna <Inspection321@gmail.com>
2025-01-14 18:16:38 +0100

    Update Header.tsx

--- msngr-ui/src/components/header/Header.tsx
+++ msngr-ui/src/components/header/Header.tsx
@@ -1,31 +1,26 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Container from "@mui/material/Container";
-import Branding from "./Branding";
-import MobileNavigation from "./mobile/MobileNavigation";
-import MobileBranding from "./mobile/MobileBranding";
-import Navigation from "./Navigation";
-import Settings from "./Settings";
-import { useReactiveVar } from "@apollo/client";
-import { authenticatedVar } from "../../constants/authenticated";
-import { Page } from "../../interfaces/page.interface";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Branding from './Branding';
+import MobileNavigation from './mobile/MobileNavigation';
+import MobileBranding from './mobile/MobileBranding';
+import Navigation from './Navigation';
+import Settings from './Settings';
+import { useReactiveVar } from '@apollo/client';
+import { authenticatedVar } from '../../constants/authenticated';
+import { Page } from '../../interfaces/page.interface';
 
-const pages: Page[] = [
-  {
-    title: "Home",
-    path: "/",
-  },
-];
+const pages: Page[] = [];
 
 const unauthenticatedPages: Page[] = [
   {
-    title: "Login",
-    path: "/login",
+    title: 'Login',
+    path: '/login',
   },
   {
-    title: "Signup",
-    path: "/signup",
+    title: 'Signup',
+    path: '/signup',
   },
 ];
 
